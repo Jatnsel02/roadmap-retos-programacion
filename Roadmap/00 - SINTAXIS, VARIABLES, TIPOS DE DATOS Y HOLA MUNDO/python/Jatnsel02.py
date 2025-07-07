60d0c4538d80f74d2f421ce09dd5cef7d02411bc
Jatnsel Reymond Alcantara Calderon <74470697+Jatnsel02@users.noreply.github.com>
2025-07-07 13:37:43 -0400

    Jatnsel02

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jatnsel02.py
@@ -0,0 +1,28 @@
+# https://www.python.org/
+
+# Este es un tipo de comentario
+
+"""
+Esta es la doble comilla para comentar varias lineas
+"""
+'''
+Esta es la comilla simple que se puede usar para 
+comentar varias lineas igual
+'''
+#Variable
+my_variable = "JRAC"
+my_variable = "Nuevo valor"
+
+#Constantes
+MY_CONSTANT = "Mi constante" # Constante por convencion ya que no hay constantes en Python
+
+#Variables Datos Primitivos
+
+my_int = 1 # Numero entero
+my_float = 3.14 # nuemeros decimales
+my_bool = True # Logica boolea
+my_bool = False # Logica boolea
+my_string = "Hola Python"
+my_other_string = 'Bienvenido '
+
+print(my_string)
